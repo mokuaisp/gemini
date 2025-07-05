602607a2decc83d346fd19b4b03f6a2c88b7421d
mokuaisp <121851682+mokuaisp@users.noreply.github.com>
2025-07-06 01:15:54 +0800

    Use Gemini Blance

--- src/worker.mjs
+++ src/worker.mjs
@@ -64,9 +64,9 @@ const handleOPTIONS = async () => {
   });
 };
 
-const BASE_URL = "https://generativelanguage.googleapis.com";
+// const BASE_URL = "https://generativelanguage.googleapis.com";
 const API_VERSION = "v1beta";
-// const BASE_URL = "https://gb.131417917.xyz"
+const BASE_URL = "https://gb.131417917.xyz"
 
 // https://github.com/google-gemini/generative-ai-js/blob/cf223ff4a1ee5a2d944c53cddb8976136382bee6/src/requests/request.ts#L71
 const API_CLIENT = "genai-js/0.21.0"; // npm view @google/generative-ai version
